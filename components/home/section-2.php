429e071c179189e3316ffd46ad47937ffefdd46d
Siddharth Asthana <59534733+sidmrlazy@users.noreply.github.com>
2023-08-02 16:51:20 +0530

    modified:   assets/styles.css modified:   components/home/section-2.php

--- components/home/section-2.php
+++ components/home/section-2.php
@@ -1,18 +1,18 @@
 <div class="container">
     <div class="homepage-section-2-row">
         <div class="homepage-section-2-card" id="blue">
-            <h2>Lorem, ipsum.</h2>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt, dignissimos.</p>
+            <h2>Excellence in Medical Education</h2>
+            <p>Excellence in nursing education, compassionate care, and medical empowerment.</p>
         </div>
 
         <div class="homepage-section-2-card" id="yellow">
-            <h2>Lorem, ipsum.</h2>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt, dignissimos.</p>
+            <h2>Nurturing Healthcare Leaders for Tomorrow"</h2>
+            <p>Medical excellence thrives at Shambhunath Memorial Nursing College</p>
         </div>
 
         <div class="homepage-section-2-card" id="blue">
-            <h2>Lorem, ipsum.</h2>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt, dignissimos.</p>
+            <h2>A Beacon of Excellence in Medical Education</h2>
+            <p>Your Pathway to Rewarding Healthcare Careers</p>
         </div>
     </div>
 </div>
